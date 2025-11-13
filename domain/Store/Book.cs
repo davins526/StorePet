61082a0902c2f5d307a69c49a7d46d6bc848b646
Davins <danie2dickson3@gmail.com>
2025-11-13 22:52:15 +0500

    Create  Class book

---
+++ domain/Store/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Store
+{
+    public class Book
+    {
+        public int Id { get; }
+        string Title { get; }
+        public Book(int id, string title)
+        {
+            Id = id;
+            Title = title;
+        }
+    }
+
+}
+
